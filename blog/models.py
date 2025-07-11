1b10375c60965ee76eb0d12199786654a1d96563
Danylo-D87 <quaestor88@gmail.com>
2025-07-11 12:55:22 +0300

    added models for yser and blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Article(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="articles",
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Photo(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="photos")
+    image = models.ImageField(upload_to="article_photos")
+
+    def __str__(self):
+        return f"Photo for article: {self.article.title}"
