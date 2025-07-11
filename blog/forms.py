7c8229a83046fb6840f1e41061a950affda1f363
Danylo-D87 <quaestor88@gmail.com>
2025-07-11 15:07:02 +0300

    remove comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,3 @@
-# articles/forms.py
 from django import forms
 from django.forms import inlineformset_factory
 from .models import Article, Photo
@@ -6,17 +5,13 @@ from .models import Article, Photo
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'content'] # 'author' буде встановлено автоматично у view
+        fields = ['title', 'content']
 
-# Формсет для фотографій
-# extra=1 означає, що за замовчуванням буде одне порожнє поле для завантаження нового фото
-# can_delete=True дозволяє видаляти існуючі фотографії
-# max_num=5 обмежує кількість фото до 5 (можна змінити)
 PhotoFormSet = inlineformset_factory(
     Article,
     Photo,
     fields=['image'],
-    extra=1, # Завжди показувати хоча б одне порожнє поле для нового фото
-    can_delete=True, # Дозволити видаляти існуючі фото
-    max_num=5, # Максимальна кількість фото на статтю
+    extra=1,
+    can_delete=True,
+    max_num=5,
 )
--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,45 @@
-# articles/views.py
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db import transaction  # Для атомарних операцій з базою даних
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
+from django.db import transaction
 
-from .models import Article, Photo
+from .models import Article
 from .forms import ArticleForm, PhotoFormSet
 
 
-# Сторінка перегляду всіх статей
 class ArticleListView(ListView):
     model = Article
     template_name = 'blog/article_list.html'
     context_object_name = 'articles'
-    paginate_by = 10  # Додаємо пагінацію як "плюс"
+    paginate_by = 10
 
 
-# Сторінка перегляду конкретної статті
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
     context_object_name = 'article'
 
 
-# Створення нової статті
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     form_class = ArticleForm
     template_name = 'blog/article_form.html'
-    success_url = reverse_lazy('article_list')  # Перенаправлення після успішного створення
+    success_url = reverse_lazy('article_list')
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if self.request.POST:
-            # При POST-запиті ініціалізуємо формсет з даними
             data['photo_formset'] = PhotoFormSet(self.request.POST, self.request.FILES)
         else:
-            # При GET-запиті ініціалізуємо порожній формсет
             data['photo_formset'] = PhotoFormSet()
         return data
 
@@ -45,26 +47,18 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
         context = self.get_context_data()
         photo_formset = context['photo_formset']
 
-        # Використовуємо транзакцію для атомарного збереження:
-        # якщо щось піде не так з фото, стаття також не буде збережена.
         with transaction.atomic():
-            form.instance.author = self.request.user  # Присвоюємо поточного користувача як автора
-            self.object = form.save()  # Зберігаємо статтю
+            form.instance.author = self.request.user
+            self.object = form.save()
 
-            # Зберігаємо фото, якщо формсет валідний
             if photo_formset.is_valid():
-                photo_formset.instance = self.object  # Прив'язуємо фото до щойно створеної статті
+                photo_formset.instance = self.object
                 photo_formset.save()
             else:
-                # Якщо формсет невалідний, але форма статті валідна,
-                # це може бути ознакою того, що є помилки у завантажених фото.
-                # Для цього прикладу ми дозволяємо статтю без фото,
-                # але ви можете додати додаткову логіку або вивести помилки формсету.
                 pass
         return super().form_valid(form)
 
 
-# Редагування статті
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     form_class = ArticleForm
@@ -75,10 +69,8 @@ class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if self.request.POST:
-            # При POST-запиті ініціалізуємо формсет з даними та існуючим об'єктом
             data['photo_formset'] = PhotoFormSet(self.request.POST, self.request.FILES, instance=self.object)
         else:
-            # При GET-запиті ініціалізуємо формсет з існуючими фотографіями статті
             data['photo_formset'] = PhotoFormSet(instance=self.object)
         return data
 
@@ -87,18 +79,17 @@ class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         photo_formset = context['photo_formset']
 
         with transaction.atomic():
-            self.object = form.save()  # Зберігаємо зміни у статті
+            self.object = form.save()
 
             if photo_formset.is_valid():
                 photo_formset.instance = self.object
-                photo_formset.save()  # Зберігаємо зміни у фотографіях (додавання, редагування, видалення)
+                photo_formset.save()
             else:
-                pass  # Обробка помилок формсету, якщо потрібно
+                pass
 
         return super().form_valid(form)
 
     def test_func(self):
-        # Перевірка, чи поточний користувач є автором статті
         article = self.get_object()
         return article.author == self.request.user
 
@@ -111,6 +102,5 @@ class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     success_url = reverse_lazy('article_list')
 
     def test_func(self):
-        # Перевірка, чи поточний користувач є автором статті
         article = self.get_object()
         return article.author == self.request.user
