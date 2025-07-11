8056eca32ab07c1dc6bb7927013e0300006e3d72
Danylo-D87 <quaestor88@gmail.com>
2025-07-11 13:49:11 +0300

    fix users forms

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["first_name", "username", "email", "password1", "password2"]
+        fields = ["first_name", "username", "password1", "password2"]
 
 
 class UserLoginForm(AuthenticationForm):
